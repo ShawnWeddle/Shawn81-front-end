b49bdfaa3fb7820c772c10edea0dba7bf303ffc1
unknown <marlinzeah@gmail.com>
2023-02-06 21:23:13 -0500

    Loading page update

--- src/pages/loadingPage.tsx
+++ src/pages/loadingPage.tsx
@@ -1,7 +1,22 @@
 interface LoadingPageProps {}
 
 const LoadingPage = (props: LoadingPageProps) => {
-  return <div className="loading-page"> LOADING... </div>;
+  return (
+    <div className="flex-wrapper-center">
+      <div className="loading-page">
+        <span className="loading-page-letter">L</span>
+        <span className="loading-page-letter">O</span>
+        <span className="loading-page-letter">A</span>
+        <span className="loading-page-letter">D</span>
+        <span className="loading-page-letter">I</span>
+        <span className="loading-page-letter">N</span>
+        <span className="loading-page-letter">G</span>
+        <span className="loading-page-letter">.</span>
+        <span className="loading-page-letter">.</span>
+        <span className="loading-page-letter">.</span>
+      </div>
+    </div>
+  );
 };
 
 export default LoadingPage;
